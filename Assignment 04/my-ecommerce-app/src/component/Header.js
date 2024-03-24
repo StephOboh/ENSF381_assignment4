6617dcc1b1493808a5f26b9b6209bb2f0f1dc15d
Sarah Y <sarahyip64@gmail.com>
2024-03-24 10:14:52 -0600

    add header component with styling sheet

--- Assignment 04/my-ecommerce-app/src/component/Header.js
+++ Assignment 04/my-ecommerce-app/src/component/Header.js
@@ -1,9 +1,28 @@
+/*
+=========================================================
+Name        : Header.js
+Assignment  : Assignment 4
+Author(s)   : Sarah Yip (UCID: 30174831), Stephenie Oboh (UCID: 30151629)
+Submission  : March 24, 2024
+Description : Header Component for Company Website
+=========================================================
+*/
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './Header.css'; // import link to styling sheet 
+
 
 function Header() {
   return (
-    <header>
-      {/* Your header content here */}
+    <header className='header'>
+      <div className='logoHeader'>
+        <img src = "public/images/logo.png" alt = "Company logo"></img></div>
+      <div className='company-nameHeaeder'>Company Name</div>
+      <div className='navHeader'>
+        <a href='Homepage.js'>Home</a>
+        <a href='Productpage.js'>Products</a>
+        <a href='Login.js'>Login</a>
+      </div>
     </header>
   );
 }
---
+++ Assignment 04/my-ecommerce-app/src/component/Homepage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <header>
+      {/* Your header content here */}
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  );
+}
+
+export default Header;
