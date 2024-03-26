a7837e7b943d707664311fc57b8e337d96579ec7
StephO <stephenie.oboh@ucalgary.ca>
2024-03-25 21:13:31 -0600

    making description show only when hover over product name

--- Assignment-04/my-ecommerce-app/src/component/ProductItem.js
+++ Assignment-04/my-ecommerce-app/src/component/ProductItem.js
@@ -27,16 +27,27 @@ const ProductItem = ({ product, addToCart }) => {
   };
 
   return (
-    <div className="product-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {/* Display product image */}
+    // <div className="product-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    //   {/* Display product image */}
+    //   <img src={image} alt={name} style={{ width: '200px', height: '200px' }} />
+    //   <div>
+    //     {/* Display product details */}
+    //     <h3>{name}</h3>
+    //     <p>Price: ${price}</p>
+    //     {/* Show description on hover */}
+    //     {showDescription && <p>Description: {description}</p>}
+    //     {/* Add to Cart button */}
+    //     <button onClick={handleAddToCart}>Add to Cart</button>
+    //   </div>
+    // </div>
+
+    <div className="product-item">
       <img src={image} alt={name} style={{ width: '200px', height: '200px' }} />
       <div>
-        {/* Display product details */}
-        <h3>{name}</h3>
+        <h3 onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{name}</h3>
         <p>Price: ${price}</p>
-        {/* Show description on hover */}
+        {/* Show description only when showDescription is true */}
         {showDescription && <p>Description: {description}</p>}
-        {/* Add to Cart button */}
         <button onClick={handleAddToCart}>Add to Cart</button>
       </div>
     </div>
