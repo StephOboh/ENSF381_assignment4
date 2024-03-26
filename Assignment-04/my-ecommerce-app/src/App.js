6df2c6672a521946c35082168c8ec18f95318bbe
StephO <stephenie.oboh@ucalgary.ca>
2024-03-25 20:37:02 -0600

    removing comments

--- Assignment-04/my-ecommerce-app/src/App.js
+++ Assignment-04/my-ecommerce-app/src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import Homepage from './component/Homepage'
-// import Productpage from './component/Productpage'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path ="/" element ={<Homepage></Homepage>}></Route>
-//         <Route path="/Productpage" element={<Productpage></Productpage>}></Route>
-//       </Routes>
-//     </Router>
-//   );}
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Homepage from './component/Homepage';
